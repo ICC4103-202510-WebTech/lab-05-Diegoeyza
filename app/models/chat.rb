3903d2e700a4969b25e01e54a8feebd8717f1700
Diego <deyzaguirre1@miuandes.cl>
2025-04-30 15:59:11 -0400

    Finished doing the validations and references

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,2 +1,17 @@
 class Chat < ApplicationRecord
-end
+    belongs_to :sender, class_name: "User"
+    belongs_to :receiver, class_name: "User"
+    has_many :messages, dependent: :destroy
+  
+    validates :sender_id, presence: true
+    validates :receiver_id, presence: true
+    validate :sender_and_receiver_must_be_different
+  
+    private
+    def sender_and_receiver_must_be_different
+      if sender_id == receiver_id
+        errors.add(:receiver_id, "The chat cannot be between you and yourself xD")
+      end
+    end
+  end
+  
